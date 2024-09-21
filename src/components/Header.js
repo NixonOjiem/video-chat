da61702f7faad53ca2f75ecaa7c86d1100fe0f1e
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-21 09:43:39 +0300

    added the link to github

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <div className='Navigation-bar'>
         <h3 className='App-name'>Video chat</h3>
         <ul className='List-items'>
-            <li><a href=''>Github</a></li>
+            <li><a href='https://github.com/NixonOjiem/video-chat'>Github</a></li>
             <li><a href='https://myportfolio-cde82.web.app/'>Portfolio</a></li>
         </ul>
     </div>
